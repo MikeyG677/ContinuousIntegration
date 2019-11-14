00b7db2a46fe71b4728e40a9cb73b3e13fe096ab
MichaelGallegos <magallegos56@gmail.com>
2019-11-14 13:40:20 -0500

    create test that fails

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -2,10 +2,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class calculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     public void testAdd(){
-        Calculator calculator = new Calculator();
         Assertions.assertEquals(9,calculator.add(4,5));
+    }
+
+    @Test
+    public void failAdd(){
+        Assertions.assertEquals(8,calculator.add(4,5));
     }
 }
